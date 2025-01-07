9aea2379e8dffeb22879c1dafa2f613ba6094038
VASANTHAKUMAR SEKAR <vasanthakumar.s@datayaan.com>
2025-01-07 18:09:01 +0530

    Add files via upload

--- Vehicle.java
+++ Vehicle.java
@@ -1,85 +1,62 @@
-package Yaantrac;
-
-import java.util.HashMap;
-import java.util.Map;
+package YaantracAutomation;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
 
-public class Vehicle {
-
-	public static void main(String[] args) throws InterruptedException {
-		// Notification Handle
-		ChromeOptions options = new ChromeOptions();
-		Map<String, Object> prefs = new HashMap<String, Object>();
-		prefs.put("profile.default_content_setting_values.notifications", 1);
-		options.setExperimentalOption("prefs", prefs);
-
-		// To launch Browers
-
-		WebDriver driver = new ChromeDriver(options);
-		driver.get("https://dev-yaan.yaantrac.com/");
-		Thread.sleep(2000);
-		driver.manage().window().maximize();
-
-		// login
-		driver.findElement(By.xpath("//button[normalize-space(text())='Login']")).click();
-		Thread.sleep(2000);
-		driver.findElement(By.id("username")).sendKeys("demo");
-		Thread.sleep(2000);
-		driver.findElement(By.id("password")).sendKeys("Demo@12345!");
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//button[normalize-space(text())='Login']")).click();
-		Thread.sleep(3000);
-		
-		//Configuration
-		WebElement Configuration = driver.findElement(By.xpath("//*[@aria-label='Configurations']"));
-		Configuration.click();
-		Thread.sleep(2000);
-		
-		//Vehicle
-		WebElement vehicle = driver.findElement(By.xpath(" //*[normalize-space(text())='Vehicle']"));
-		vehicle.click();
-		Thread.sleep(2000);
-		
-		//VehicleSearch
-		WebElement VehicleSearch = driver.findElement(By.xpath("//*[@name='search']"));
-		VehicleSearch.sendKeys("TN02BF1373");
-		Thread.sleep(3000);
-		//ToClickVehicleWidget
-		WebElement Vehiclewidget = driver.findElement(By.xpath("//div[@class='vehicle-card MuiBox-root css-0']"));
-		Vehiclewidget.click();
-		Thread.sleep(2000);
-		//ToUpdateVehicleDetails
-		WebElement UpdateVehicle = driver.findElement(By.xpath(" //*[normalize-space(text())='Update']"));
-		UpdateVehicle.click();
-		Thread.sleep(2000);
-		WebElement FreeWay = driver.findElement(By.xpath("//input[@name='freeWay']"));
-//		FreeWay.clear();
-		for (int i = 0; i < 5; i++) {
-			FreeWay.sendKeys(Keys.BACK_SPACE);
-		}		
-		Thread.sleep(2000);
-		FreeWay.sendKeys("110");
-		Thread.sleep(2000);
-		WebElement NonFreeWay = driver.findElement(By.xpath("//input[@name='nonFreeWay']"));
-		
-		for (int i = 0; i < 5; i++) {
-			NonFreeWay.sendKeys(Keys.BACK_SPACE);
-		}	
-		Thread.sleep(2000);
-		NonFreeWay.sendKeys("85");
-		Thread.sleep(2000);
-		WebElement ToUpdate = driver.findElement(By.xpath("( //*[normalize-space(text())='Update'])[3]"));
-		ToUpdate.click();
+public class Vehicle extends Driver {
 	
-		
-
+	//Vehicle
+	@Test(priority = 153, description = "The testcase for clicking the Vehicle Configuration")
+	public void ClickVehicle() throws InterruptedException {
+	WebElement vehicle = driver.findElement(By.xpath(" //*[normalize-space(text())='Vehicle']"));
+	vehicle.click();
+	Thread.sleep(2000);
 	}
+	//VehicleSearch
+	@Test(priority = 154, description = "The testcase for Searching the vehicle")
+	public void SearchVehicle() throws InterruptedException {
+	WebElement VehicleSearch = driver.findElement(By.xpath("//*[@name='search']"));
+	VehicleSearch.sendKeys("TN02BF1373");
+	Thread.sleep(3000);
+	}
+	//ToClickVehicleWidget
+	@Test(priority = 155, description = "The testcase for clicking the vehicle")
+	public void ClickVehiclewidget() throws InterruptedException {
+	WebElement Vehiclewidget = driver.findElement(By.xpath("//div[@class='vehicle-card MuiBox-root css-0']"));
+	Vehiclewidget.click();
+	Thread.sleep(2000);
+	}
+	//ToUpdateVehicleDetails
+	@Test(priority = 156, description = "The testcase for vehicle update")
+	public void VehicleUpdate() throws InterruptedException {
+	WebElement UpdateVehicle = driver.findElement(By.xpath(" //*[normalize-space(text())='Update']"));
+	UpdateVehicle.click();
+	Thread.sleep(2000);
+	WebElement FreeWay = driver.findElement(By.xpath("//input[@name='freeWay']"));
+	
+//	FreeWay.clear();
+	for (int i = 0; i < 5; i++) {
+		FreeWay.sendKeys(Keys.BACK_SPACE);
+	}		
+	Thread.sleep(2000);
+	FreeWay.sendKeys("110");
+	Thread.sleep(2000);
+	WebElement NonFreeWay = driver.findElement(By.xpath("//input[@name='nonFreeWay']"));
+	
+	for (int i = 0; i < 5; i++) {
+		NonFreeWay.sendKeys(Keys.BACK_SPACE);
+	}	
+	Thread.sleep(2000);
+	NonFreeWay.sendKeys("85");
+	Thread.sleep(2000);
+	WebElement ToUpdate = driver.findElement(By.xpath("( //*[normalize-space(text())='Update'])[2]"));
+	ToUpdate.click();
+	}
+	
+
+
 
 }
+
